0091656e35b428a6446bf5826c5050b4e941dd0e
lloria91 <lloria@axway.com>
2018-10-21 20:32:54 +0200

    React lifecycle part

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Intro from './intro'
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      name: 'Manny'
+    }
+  }
+
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2>{this.state.name}, welcome to React.</h2>
+        </div>
+        <Intro name={this.state.name} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
